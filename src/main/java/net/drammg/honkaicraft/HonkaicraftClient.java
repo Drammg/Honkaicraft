7c79e22d6dffc39b6d6e927f4b8fe562a2624429
drammg <matthieu.barbaud@orange.fr>
2022-08-07 15:20:48 +0200

    1

---
+++ src/main/java/net/drammg/honkaicraft/HonkaicraftClient.java
@@ -0,0 +1,10 @@
+package net.drammg.honkaicraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HonkaicraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
