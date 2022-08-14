58656ec5f80584dbe77dc39044310802bb05374d
drammg <matthieu.barbaud@orange.fr>
2022-08-14 22:43:10 +0200

    Added Eight Ball Tooltip

--- src/main/java/net/drammg/honkaicraft/item/custom/EightBallItem.java
+++ src/main/java/net/drammg/honkaicraft/item/custom/EightBallItem.java
@@ -1,14 +1,19 @@
 package net.drammg.honkaicraft.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Set;
 
 public class EightBallItem extends Item {
@@ -27,6 +32,16 @@ public class EightBallItem extends Item {
         }
 
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()){
+            tooltip.add(Text.literal("Right Click for a random number !").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Thanks Kaupenjoe !").formatted(Formatting.ITALIC));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
     }
 
     private void outputRandomNumber(PlayerEntity player) {
